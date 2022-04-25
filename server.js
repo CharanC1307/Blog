f93951fb412f920d3deef29d3393cf71ab077b58
CharanC1307 <66274903+CharanC1307@users.noreply.github.com>
2022-04-24 21:52:17 -0500

    Fixed Automattic Env Var For Port

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const articleRouter=require('./routes/articles')
 const methodOverride = require('method-override')
 const path = require('path')
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 8080
 const URI = process.env.MONGODB_URI
 const app=express()
 
